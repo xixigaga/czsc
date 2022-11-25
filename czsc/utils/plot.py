f8a4b47201ed86fd0e95a4f0aece93ed2f198680
Jared <xixigaga@sina.com>
2022-11-25 14:45:12 +0800

    增加画图

--- czsc/utils/plot.py
+++ czsc/utils/plot.py
@@ -54,14 +54,16 @@ def ka_to_image(ka, file_image, mav=(5, 20, 120, 250), max_k_count=1000, dpi=50)
     fig.savefig(fname=file_image, dpi=dpi, bbox_inches='tight')
     plt.close()
 
-def ka_to_image2(ka, file_image, zs=None, mav=(5, 20), max_k_count=1000, dpi=50):
+def ka_to_image2(ka, file_image, zs=None, macd=None,max_k_count=1000,
+            mav=(5, 20),  dpi=70):
     """绘制 ka，保存到 file_image"""
     df = ka.to_df(use_macd=True, ma_params=(5, 20,), max_count=max_k_count)
     df.rename({"open": "Open", "close": "Close", "high": "High",
                "low": "Low", "vol": "Volume"}, axis=1, inplace=True)
     df.index = pd.to_datetime(df['dt'])
     df = df.tail(max_k_count)
-    kwargs = dict(type='candle', mav=mav, volume=True)
+    
+    kwargs = dict(type='candle', mav=mav, volume=False,main_panel=0,)
 
     bi_xd = [
         [(x['dt'], x['bi']) for _, x in df.iterrows() if x['bi'] > 0],
@@ -80,30 +82,46 @@ def ka_to_image2(ka, file_image, zs=None, mav=(5, 20), max_k_count=1000, dpi=50)
         gridstyle='-.',
         y_on_right=False,
         marketcolors=mc)
-
+    if macd is not None:
+        macd = macd[-max_k_count:]
+        add_plot=[
+            # mpf.make_addplot(macd,type='scatter',markersize=100,panel=1,marker='^',color='r',secondary_y=False),#黑马底
+            # mpf.make_addplot(macd,panel=1,color='b',type='bar',secondary_y=True,alpha=0.9), #测试买新号
+            mpf.make_addplot(macd,panel=1,color='g',secondary_y=False,alpha=0.9),
+        ]
     mpl.rcParams['font.sans-serif'] = ['KaiTi']
     mpl.rcParams['font.serif'] = ['KaiTi']
     mpl.rcParams['font.size'] = 48
     mpl.rcParams['axes.unicode_minus'] = False
     mpl.rcParams['lines.linewidth'] = 1.0
 
-    title = '%s@%s（%s - %s）' % (ka.symbol, ka.name, df.index[0].__str__(), df.index[-1].__str__())
-    fig, axes = mpf.plot(df, 
-                        fill_between=dict(y1=zs[0],y2=zs[1] ,alpha=0.5,color='g'),
+    title = '%s（%s - %s）' % (ka.symbol, df.index[0].__str__(), df.index[-1].__str__())
+    if macd is not None:
+        fig, axes = mpf.plot(df, 
+                        addplot=add_plot,
+                        fill_between=dict(y1=zs[0][-max_k_count:],y2=zs[1][-max_k_count:] ,alpha=0.5,color='g'),
                         # fill_between=dict(y1=y1values,y2=y2value,where=where_values,alpha=0.5,color='g')
                         columns=['Open', 'High', 'Low', 'Close', 'Volume'], style=s,
                          title=title, ylabel='K线', ylabel_lower='成交量', **kwargs,
                          alines=dict(alines=bi_xd, colors=['r', 'g'], linewidths=2, alpha=0.35),
                          returnfig=True,
-                         
+                         )
+    else:
+        fig, axes = mpf.plot(df, 
+                        fill_between=dict(y1=zs[0][-max_k_count:],y2=zs[1][-max_k_count:] ,alpha=0.5,color='g'),
+                        # fill_between=dict(y1=y1values,y2=y2value,where=where_values,alpha=0.5,color='g')
+                        columns=['Open', 'High', 'Low', 'Close', 'Volume'], style=s,
+                         title=title, ylabel='K线', ylabel_lower='成交量', **kwargs,
+                         alines=dict(alines=bi_xd, colors=['r', 'g'], linewidths=2, alpha=0.35),
+                         returnfig=True,
                          )
 
     w = len(df) * 0.15
     fig.set_size_inches(w, 30)
     ax = plt.gca()
     ax.set_xbound(-1, len(df) + 1)
-    plt.show()
-    # fig.savefig(fname=file_image, dpi=dpi, bbox_inches='tight')
+    # plt.show()
+    fig.savefig(fname=file_image, dpi=dpi, bbox_inches='tight')
     
 
 def ka_to_echarts(ka, width: str = "1500px", height: str = '800px'):
--- test/test_plot.py
+++ test/test_plot.py
@@ -4,7 +4,7 @@ import warnings
 
 sys.path.insert(0, '.')
 sys.path.insert(0, '..')
-
+import talib
 import os
 import pandas as pd
 import numpy as np
@@ -55,9 +55,11 @@ def checkType():
             形态       背驰类型
     返回有： aAbBc   b/c  a/bc   A/B
             aAb     ab
+            abc     ac
     '''
     return
-def zs2List(zslist,kbars):
+
+def zs2ListPic(zslist,kbars):
     '''
     将多个中枢转成list
     '''
@@ -91,11 +93,13 @@ def zs2List(zslist,kbars):
 def qaCzsc():
     end_eDate = '2022-11-03'
     end_sDate = '2022-09-01'
-    end_sDate = '2022-10-14'
+    end_sDate = '2022-10-12'
+    code='000010'
+
     start_date = QA_util_get_pre_trade_date(end_sDate,250) #近一年的数据
     start_index = trade_date_sse.index(end_sDate)
     end_index = trade_date_sse.index(end_eDate,1)
-    te = QA.QA_fetch_stock_day_adv('000010',start_date,end_eDate).to_qfq()
+    te = QA.QA_fetch_stock_day_adv(code,start_date,end_eDate).to_qfq()
     testData = te.data.round(2)
     for i in range(start_index,end_index):
         tempData = testData[testData.index.get_level_values(0)<=trade_date_sse[i]]
@@ -103,47 +107,67 @@ def qaCzsc():
         bars.rename(columns={'code': 'symbol', 'date': 'dt','volume':'vol'}, inplace=True)
         ka = KlineAnalyze(bars,)
         zslist = find_zs(ka.bi_list)
-        zs2 = zs2List(zslist,bars)
-        ka_to_image2(ka,'d:\\abc.jpg',zs= zs2)
-        exit()
-        
-        xd_zslist2 = single_Check(bars,zslist) #找出最近
+        if ka.bi_list[-1]['fx_mark']=='d' and str(ka.bi_list[-1]['end_dt'])[:10]==trade_date_sse[i]: #大趋势刚出现底分型，
+            # todo 当前出现背驰（多种处理逻辑，1、aAbBc 2、aAb  3、abc）
+            # 第一买点 第二买点 第三买卖点逻辑处理
+            # 根据背离段进次级别验证是否也出现背驰现象
+            print(trade_date_sse[i])
+            zsPic2 = zs2ListPic(zslist,bars)
+            ka_to_image2(ka,'d:\\'+code+str(trade_date_sse[i])[5:10]+'.jpg',zs= zsPic2)
+
+            min60 = QA.QA_fetch_stock_min_adv(code,
+                        str(ka.bi_list[-4]['dt'])[:10], # str(ka.bi_list[-2]['dt'])[:10],
+                        str(ka.bi_list[-1]['dt'])[:10],frequence='30min').to_qfq()
+            #待添加
+            num = len(min60[min60.index.get_level_values(0)>ka.bi_list[-2]['dt']])+1
+            # 增加数量
+            #  macd将背驰数据绘制到图中
+            min_bars = min60.data.round(2).reset_index()
+            min_bars.rename(columns={'code': 'symbol', 'datetime': 'dt','volume':'vol'}, inplace=True)
+            diff, dea, macd = talib.MACD(min60['close'], fastperiod=12, slowperiod=26, signalperiod=9)
+
+            min_ka = KlineAnalyze(min_bars,use_xd=False,) # 次级别需要合并笔
+            bi_zslist = find_zs(min_ka.bi_list) #获取中枢 用以计算最后次级别的时间长度 看局部中枢背离也可以
+            # xd_zslist2 = single_Check(min_bars,bi_zslist)
+            bi_zsPic2 = zs2ListPic(bi_zslist,min_bars)
+            ka_to_image2(min_ka,'d:\\'+code+str(trade_date_sse[i])[5:10]+'min.jpg',zs= bi_zsPic2,macd=macd,max_k_count=num)
+            # exit()
+        # xd_zslist2 = single_Check(bars,zslist) #找出最近
         # 1、当前出现低分型
         # 2、分型之后
-    exit()
     '''
     1、确定是否出现日分型
     2、如果出现日分型后,验证是否到底,通过区间套方法:验证次级别是否出现背驰
     3、次级别背驰的多种验证方法
     '''
-    te = QA.QA_fetch_stock_day_adv('000001','2022-01-01',end_eDate).to_qfq()
-    bars = te.data.round(2).reset_index()
-    bars.rename(columns={'code': 'symbol', 'date': 'dt'}, inplace=True)
-    ka = KlineAnalyze(bars,)
-    zslist = find_zs(ka.bi_list)
+    # te = QA.QA_fetch_stock_day_adv(code,'2022-01-01',end_eDate).to_qfq()
+    # bars = te.data.round(2).reset_index()
+    # bars.rename(columns={'code': 'symbol', 'date': 'dt'}, inplace=True)
+    # ka = KlineAnalyze(bars,)
+    # zslist = find_zs(ka.bi_list)
 
-    if ka.bi_list[-1]['fx_mark']=='d' and str(ka.bi_list[-1]['end_dt'])[:10]==end_date: #大趋势刚出现底分型，
-        # todo 当前出现背驰（多种处理逻辑，1、aAbBc 2、aAb  3、abc）
-        # 第一买点 第二买点 第三买卖点逻辑处理
-        # 根据背离段进次级别验证是否也出现背驰现象
-        print(end_eDate)
-    xd_zslist2 = single_Check(bars,zslist) #找出最近
-    # 日线出现背驰
-    # 查看分钟线是否也背驰并且结束
-    '''
-    当下处于中枢最后一笔
-    获取次级别中枢前一笔+中枢+最后一笔
-    f=open("d:\\30bi.txt","w")
-    f.write(str(min_ka.bi_list))
-    f.close()
-    '''
-    min60 = QA.QA_fetch_stock_min_adv('000001','2022-09-15','2022-11-04',frequence='30min').to_qfq()
-    min_bars = min60.data.round(2).reset_index()
-    min_bars.rename(columns={'code': 'symbol', 'datetime': 'dt'}, inplace=True)
+    # if ka.bi_list[-1]['fx_mark']=='d' and str(ka.bi_list[-1]['end_dt'])[:10]==end_sDate: #大趋势刚出现底分型，
+    #     # todo 当前出现背驰（多种处理逻辑，1、aAbBc 2、aAb  3、abc）
+    #     # 第一买点 第二买点 第三买卖点逻辑处理
+    #     # 根据背离段进次级别验证是否也出现背驰现象
+    #     print(end_eDate)
+    # xd_zslist2 = single_Check(bars,zslist) #找出最近
+    # # 日线出现背驰
+    # # 查看分钟线是否也背驰并且结束
+    # '''
+    # 当下处于中枢最后一笔
+    # 获取次级别中枢前一笔+中枢+最后一笔
+    # f=open("d:\\30bi.txt","w")
+    # f.write(str(min_ka.bi_list))
+    # f.close()
+    # '''
+    # min60 = QA.QA_fetch_stock_min_adv(code,'2022-09-15','2022-11-04',frequence='30min').to_qfq()
+    # min_bars = min60.data.round(2).reset_index()
+    # min_bars.rename(columns={'code': 'symbol', 'datetime': 'dt'}, inplace=True)
 
-    min_ka = KlineAnalyze(min_bars,use_xd=False,) # 次级别需要合并笔
-    bi_zslist = find_zs(min_ka.bi_list) #获取中枢 用以计算最后次级别的时间长度 看局部中枢背离也可以
-    xd_zslist2 = single_Check(min_bars,bi_zslist)
+    # min_ka = KlineAnalyze(min_bars,use_xd=False,) # 次级别需要合并笔
+    # bi_zslist = find_zs(min_ka.bi_list) #获取中枢 用以计算最后次级别的时间长度 看局部中枢背离也可以
+    # xd_zslist2 = single_Check(min_bars,bi_zslist)
     return bi_zslist
 
 if __name__ == '__main__':
